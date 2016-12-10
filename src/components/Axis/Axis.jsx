2f3a3b76d694808a6f433aa53f79bd6c6023121e
Kacper Golinski <kacper.golinski@gmail.com>
2016-12-10 18:00:24 +0000

    create Axis component

---
+++ src/components/Axis/Axis.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import * as d3Axis from 'd3-axis'
+import { select as d3Select } from 'd3-selection'
+
+import './Axis.css'
+
+export default class Axis extends Component {
+  componentDidMount() {
+    this.renderAxis()
+  }
+
+  componentDidUpdate() {
+    this.renderAxis()
+  }
+
+  renderAxis() {
+    const axisType = `axis${this.props.orient}`
+    const axis = d3Axis[axisType]()
+      .scale(this.props.scale)
+      .tickSize(-this.props.tickSize)
+      .tickPadding([12])
+      .ticks([4])
+
+    d3Select(this.axisElement).call(axis)
+  }
+
+  render() {
+    return (
+      <g
+        className={`Axis Axis-${this.props.orient}`}
+        ref={(el) => { this.axisElement = el; }}
+        transform={this.props.translate}
+      />
+    )
+  }
+}
