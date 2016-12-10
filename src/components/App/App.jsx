ba5c3f4687ccaa45019839d4bcae91951da02ad7
Kacper Golinski <kacper.golinski@gmail.com>
2016-12-10 19:31:36 +0000

    add Chart to App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import './App.css'
+import Chart from '../Chart'
 
 export default () =>
   <div className="App">
@@ -9,5 +10,6 @@ export default () =>
     </div>
 
     <div className="App-chart-container">
+      <Chart />
     </div>
   </div>
