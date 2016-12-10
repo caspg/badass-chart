a5382efb5026fc14329e99ebe8ad8406d295f448
Kacper Golinski <kacper.golinski@gmail.com>
2016-12-10 14:20:11 +0000

    create Chart component

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { scaleBand, scaleLinear } from 'd3-scale'
+
+import data from '../../data'
+import Axes from '../Axes'
+import Bars from '../Bars'
+
+class Chart extends Component {
+  constructor() {
+    super()
+    this.xScale = scaleBand()
+    this.yScale = scaleLinear()
+  }
+
+  render() {
+    const margins = { top: 50, right: 20, bottom: 100, left: 60 }
+    const svgDimensions = { width: 800, height: 500 }
+
+    const maxValue = Math.max(...data.map(d => d.value))
+
+    const xScale = this.xScale
+      .padding(0.5)
+      .domain(data.map(d => d.title))
+      .range([margins.left, svgDimensions.width - margins.right])
+
+    const yScale = this.yScale
+      .domain([0, maxValue])
+      .range([svgDimensions.height - margins.bottom, margins.top])
+
+    return (
+      <svg width={svgDimensions.width} height={svgDimensions.height}>
+        <Axes
+          scales={{ xScale, yScale }}
+          margins={margins}
+          svgDimensions={svgDimensions}
+        />
+        <Bars
+          scales={{ xScale, yScale }}
+          margins={margins}
+          data={data}
+          maxValue={maxValue}
+          svgDimensions={svgDimensions}
+        />
+      </svg>
+    )
+  }
+}
+
+export default Chart
---
+++ src/data.js
@@ -0,0 +1,13 @@
+export default [
+  { title: 'Terminator', value: 21, year: 1984 },
+  { title: 'Commando', value: 81, year: 1985 },
+  { title: 'Predator', value: 25, year: 1987 },
+  { title: 'Raw Deal', value: 26, year: 1986 },
+  { title: 'The Running Man', value: 11, year: 1987 },
+  { title: 'Total Recall', value: 44, year: 1990 },
+  { title: 'Terminator 2', value: 0, year: 1991 },
+  { title: 'Last Action Hero', value: 22, year: 1993 },
+  { title: 'True Lies', value: 51, year: 1994 },
+  { title: 'Eraser', value: 29, year: 1996 },
+  { title: 'Terminator 3', value: 2, year: 2003 },
+]
