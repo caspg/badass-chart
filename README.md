0fad556f32902572c303b236fe50e633a4a66b14
Kacper Golinski <kacper.golinski@gmail.com>
2016-12-10 13:15:19 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bad Ass chart
+
+Responsive chart example built with React and D3 v4.
---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './App.css'
+
+export default () =>
+  <div className="App">
+    <div className="App-header">
+      <h2>Welcome to the</h2><h1>BAD ASS CHART</h1>
+    </div>
+
+    <div className="App-chart-container">
+    </div>
+  </div>
